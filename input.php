4151cf10e5a466a8833a807dda16bd45469982b0
23-06 <babatenko93@mail.ru>
2019-07-03 22:53:46 +0700

    add writing GET,POST qery

--- input.php
+++ input.php
@@ -7,22 +7,25 @@ $Changed = json_decode(file_get_contents("Changed.json"), true);
 if (isset($_GET["month"]) && isset($_GET["day"])){
   $_SESSION["month"] = $_GET["month"];
   $_SESSION["day"] = $_GET["day"];
+  unset($_GET);
 };
 
 if (isset($_POST["submit"]) && !empty($_POST["phone"])){
   if (!empty($_SESSION)) {
-    $Changed[count($Changed)] = array('0'=>'2', '1'=>'3', '3'=>'+79235946626');
+    $Changed[count($Changed)] = array('0'=>strval($_SESSION["month"]), '1'=>strval($_SESSION["day"]), '3'=>strval($_POST["phone"]));
     file_put_contents('Changed.json', json_encode($Changed));
-    unset($_SESSION); unset($_GET); unset($_POST);
+    unset($_SESSION); unset($_POST);
   }else{
     echo ("<script type='text/javascript'> alert('Выберите дату.');</script>");
-    unset($_SESSION); unset($_GET); unset($_POST);
+    unset($_SESSION); unset($_POST);
   }
 }else{
   if (isset($_POST["submit"]) && empty($_POST["phone"])){
     echo ("<script type='text/javascript'> alert('Заполните номер телефона для бронирования даты.');</script>");
-    unset($_SESSION); unset($_GET); unset($_POST);
+    unset($_SESSION); unset($_POST);
   }
 }
+
+var_dump($Changed);
 
 ?>
